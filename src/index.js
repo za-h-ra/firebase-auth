c879fdde889927132bef1f295db38fed686ff475
zahrakhadijha <zahra.khan1@icloud.com>
2022-08-11 14:24:01 -0400

    [ADD] firebase state change function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import { initalizeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const firebaseApp = initalizeApp({
   apiKey: "AIzaSyCYJIkOsZQr_yVHXcwgYHyDPbXXsfLUhJo",
   authDomain: "my-auth-app-b94e1.firebaseapp.com",
   projectId: "my-auth-app-b94e1",
   storageBucket: "my-auth-app-b94e1.appspot.com",
   messagingSenderId: "744797402913",
   appId: "1:744797402913:web:49a6780039126a9b987060",
-};
+});
 
-// Initialize Firebase
-const app = initalizeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+// Detect auth state
+onAuthStateChanged(auth, (user) => {
+  if (user !== null) {
+    console.log("Logged in!");
+  } else {
+    console.log("User not found!");
+  }
+});
