472a884177644b7109a85fad92c90a933a980505
zahrakhadijha <zahra.khan1@icloud.com>
2022-08-11 16:42:00 -0400

    [ADD] firebase config and initalizeApp

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4koinwSZIYkRP3lKzeHufr8QE0aDeok8",
+  authDomain: "zahra-auth.firebaseapp.com",
+  projectId: "zahra-auth",
+  storageBucket: "zahra-auth.appspot.com",
+  messagingSenderId: "329871843324",
+};
+
+const app = initializeApp(firebaseConfig);
